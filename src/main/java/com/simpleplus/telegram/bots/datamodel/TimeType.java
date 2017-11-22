b81b9e5407a4ad1ea0fdc04ab1f31d904c0ebb3f
Carlo Pantaleo <carlo.pantaleo@icloud.com>
2017-11-22 20:08:48 +0100

    Changed incorrect TimeTypes message.

--- src/main/java/com/simpleplus/telegram/bots/datamodel/TimeType.java
+++ src/main/java/com/simpleplus/telegram/bots/datamodel/TimeType.java
@@ -4,11 +4,11 @@ public enum TimeType {
     SUNRISE_TIME("The sun is rising.", "Sunrise"),
     SUNRISE_TIME_ANTICIPATION("The sun is rising in %d minutes.", "Sunrise"),
     SUNSET_TIME("Sunset has begun.", "Sunset"),
-    SUNSET_TIME_ANTICIPATION("Sunset has begun %d minutes ago.", "Sunset"),
+    SUNSET_TIME_ANTICIPATION("Sunset begins in %d minutes.", "Sunset"),
     CIVIL_TWILIGHT_BEGIN_TIME("Civil twilight has begun.", "Civil twilight begin"),
     CIVIL_TWILIGHT_BEGIN_TIME_ANTICIPATION("Civil twilight begins in %d minutes.", "Civil twilight begin"),
     CIVIL_TWILIGHT_END_TIME("Civil twilight has ended.", "Civil twilight end"),
-    CIVIL_TWILIGHT_END_TIME_ANTICIPATION("Civil twilight has ended %d minutes ago.", "Civil twilight end"),
+    CIVIL_TWILIGHT_END_TIME_ANTICIPATION("Civil twilight ends in %d minutes.", "Civil twilight end"),
     DEFAULT("Invalid.", "Invalid");
 
     private String message;
